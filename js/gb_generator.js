f9cc44c9f25b0ea60116781d087e3d52d25c7783
GangXtaBoii <GangXtaBoii@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-04 13:40:59 +0000

    big issue with the IE
    git-svn-id: https://plugins.svn.wordpress.org/like-button-plugin-for-wordpress/tags/1.4.5@237421 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/gb_generator.js
@@ -0,0 +1,28 @@
+// @Author: Stefan Natter
+
+function gxtb_generator() {
+	
+	var div = document.getElementById("gxtb_fb_lB_preview");
+	
+	if(document.settingpage.gxtb_fb_lB_generator_faces.checked) {
+		var faces = "true";
+	} else {
+		var faces = "false";
+	}
+
+	if(document.settingpage.gxtb_fb_lB_generator_url.value == "") {
+		var url = "http://www.gangxtaboii.com";
+	} else {
+		var url = document.settingpage.gxtb_fb_lB_generator_url.value;
+	}
+
+	if(document.settingpage.gxtb_fb_lB_generator_font.value == "") {
+		var font = "";
+	} else {
+		var font = "font=" + document.settingpage.gxtb_fb_lB_generator_font.value + "&amp;";
+	}
+	
+	div.innerHTML =
+		'<iframe src="http://www.facebook.com/plugins/like.php?href=' + url +'&layout='+ document.settingpage.gxtb_fb_lB_generator_layout.value +'&amp;show_faces='+ faces +'&amp;width='+ document.settingpage.gxtb_fb_lB_generator_width.value +'&amp;action='+ document.settingpage.gxtb_fb_lB_generator_verb.value +'&amp;' + font +'colorscheme='+ document.settingpage.gxtb_fb_lB_generator_color.value +'" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:'+ document.settingpage.gxtb_fb_lB_generator_width.value +'px; height:'+ document.settingpage.gxtb_fb_lB_generator_heigth.value +'px"></iframe>';
+
+}
