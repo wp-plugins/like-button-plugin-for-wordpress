bdc11aff3bad9593cd88c5f5a3b80463110da315
GangXtaBoii <GangXtaBoii@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-27 17:25:41 +0000

    read the changelog
    git-svn-id: https://plugins.svn.wordpress.org/like-button-plugin-for-wordpress/trunk@294143 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/gb_shortcode.php
@@ -0,0 +1,71 @@
+<?php // Do not delete these lines
+	if (!empty($_SERVER['SCRIPT_FILENAME']) && basename(__file__) == basename($_SERVER['SCRIPT_FILENAME']))
+		die ('Please do not load this page directly. Thanks!');
+?>
+<?php
+/*
++----------------------------------------------------------------+
++	Like-Button-Plugin-For-Wordpress [v4.2.4] - Like-Button-Shortcode [v0.1]
++	by Stefan Natter (http://www.gangxtaboii.com and http://www.gb-world.net)
++   required for Like-Button-Plugin-For-Wordpress and WordPress 2.7.x or higher
++----------------------------------------------------------------+
+*/
+####################################################
+####################################################
+###########								 ###########
+###########								 ###########
+###########	      ShortCode-CLASS		 ###########
+###########								 ###########
+###########								 ###########
+####################################################
+####################### by gb-world.net ############
+####################################################
+
+if (!class_exists('gxtb_fb_lB_Shortcode')) {
+class gxtb_fb_lB_Shortcode extends gxtb_fb_lB_GenerateLike {
+
+	var $java = false; # früher private
+	var $meta = false; # früher private
+
+####################################################
+####################################################
+###########								 ###########
+###########								 ###########
+###########	  LIKE-BUTTON (SHORTCODE)	 ###########
+###########				ADDING			 ###########
+###########								 ###########
+####################################################
+##################### by gb-world.net   ############
+####################################################
+function gxtb_fb_lB_Shortcode() {
+
+	$gxtb_fb_lB_settings = get_option('gxtb_fb_lB_settings');
+
+	if ( $gxtb_fb_lB_settings['activate'] )
+		add_shortcode('like', array( &$this, 'gxtb_fb_lB_function' ) );
+}
+####################################################
+####################################################
+###########								 ###########
+###########								 ###########
+###########	  LIKE-BUTTON (SHORTCODE)	 ###########
+###########				ACTION			 ###########
+###########								 ###########
+####################################################
+##################### by gb-world.net   ############
+####################################################
+function gxtb_fb_lB_function($atts) {
+
+	extract(shortcode_atts(array(
+			'url' 		=> ""
+		), $atts ));
+
+	return $this -> gxtb_fb_lB_Generate(esc_attr($url));
+	#return $this -> gxtb_fb_lB_Generate();
+}
+} // end class
+} // end if-class
+
+if (class_exists('gxtb_fb_lB_Shortcode'))
+	$gxtb_fb_lB_Shortcode = new gxtb_fb_lB_Shortcode();
+?>
